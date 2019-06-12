3df55c93556cbd580220670624317f4da0664596
Kirill Kravchuk <kkravchuk@oneweb.net>
2019-06-12 09:06:12 -0400

    created querybuilder class, attempted to add sorting

--- testgui.py
+++ testgui.py
@@ -1,60 +1,72 @@
 import PySimpleGUI as sg 
 import requests
 import json
-
+import sys
+from pprint import pprint
 url = "https://duyssearch.search.windows.net/indexes/azureblob-index/docs"     
 
 #GUI
-layout = [[sg.Text('Enter a Search Query')],      
-          [sg.Input(do_not_clear=True, key='_query_')],  
+layout = [[sg.Text('Enter a Search Query', size = (40, 1)), sg.Text('Sort by (last_modified, size):', size = (30, 1))],
+          [sg.Input(do_not_clear=True, key='_query_', size = (40, 1)), sg.Input(do_not_clear=True, key='_sortby_', size = (30, 1))],
           [sg.Checkbox('Full Lucerne Search?', key='_lucerne_')],    
           [sg.Button('Query'), sg.Exit()],
           [sg.Text('Waiting for Query...')]]      
 
 window = sg.Window('Azure Search', layout)
 
-#Hit the search API
-def hitsearch(query, search_type):
-    #Full search if checkmark is typed
-    if search_type:
-        querystring = {"api-version":"2019-05-06","search":query, "queryType":"Full"}
-    else:
-        querystring = {"api-version":"2019-05-06","search":query} 
+class QueryBuilder(object):
+    def __init__(self, query_type=None):
 
-    payload = ""
-    headers = {
-        'Content-Type': "application/json",
-        'api-key': "310C82B26640CFDBF100D8561EF5065D",
-        'cache-control': "no-cache",
-        'Postman-Token': "08038873-6b32-4f6b-bc7a-0a1d9a670a2e"
+        if query_type is None:
+            self.base = {
+                "api-version": "2019-05-06"
+            }
+        else:
+            self.base = {
+                "api-version": "2019-05-06",
+                "queryType": query_type
+            }
+        self.headers = {
+            'Content-Type': "application/json",
+            'api-key': "310C82B26640CFDBF100D8561EF5065D",
+            'cache-control': "no-cache",
+            'Postman-Token': "08038873-6b32-4f6b-bc7a-0a1d9a670a2e"
         }
 
-    response = requests.request("GET", url, data=payload, headers=headers, params=querystring)
+    def hitsearch(self, query, sortby):
+        # Full search if checkmark is typed
+        self.base.update({"search": query, "orderby": sortby})
+        payload = ""
+        response = requests.get(url=url, data=payload, headers=self.headers, params=self.base)
+        payload = json.loads(response.text)['value']
+        pprint(payload)
+        filenames = []
+        for documents in payload:
+            filenames.append({documents['metadata_storage_name'], documents["metadata_storage_last_modified"]})
+        return filenames
 
-    payload = json.loads(response.text)['value']
+    # def sorted_search(self, query):
 
-    filenames = []
-    for documents in payload:
-        filenames.append(documents['metadata_storage_name'])
-    return filenames
 
-while True:      
-    event, values = window.Read()
-    if event is None or event == 'Exit':      
-        break
-    else:
-        if values['_lucerne_']:
-            lucerne = True
-            documentsreturned = hitsearch(values['_query_'], lucerne)
+if sys.argv[1] == "hitsearch":
+    while True:
+        event, values = window.Read()
+        if event is None or event == 'Exit':
+            break
         else:
-            lucerne = False
-            documentsreturned = hitsearch(values['_query_'], lucerne)
-        
-        hits = "Number of Hits:" + str(len(documentsreturned)) 
-        sg.Popup(documentsreturned, hits, lucerne)     
-    print(event, values)
 
+            if values['_lucerne_']:
+                search_type = "Full"
+            else:
+                search_type = None
+            sortby=None
+            if values['_sortby_'] == "last_modified":
+                sortby="metadata_storage_last_modified desc"
+            qb = QueryBuilder(search_type)
+            documentsreturned = qb.hitsearch(values['_query_'], sortby)
 
-
+            hits = "Number of Hits:" + str(len(documentsreturned))
+            sg.Popup(documentsreturned, hits, search_type)
+        print(event, values)
 
 window.Close()
